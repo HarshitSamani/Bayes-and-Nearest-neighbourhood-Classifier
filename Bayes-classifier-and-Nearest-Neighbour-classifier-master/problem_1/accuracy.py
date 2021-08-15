52ba1283d7c220e03cb9a2ae7d5c1da0b21dd031
Harshit Samani <87422671+HarshitSamani@users.noreply.github.com>
2021-08-15 22:46:06 +0530

    Delete Bayes-classifier-and-Nearest-Neighbour-classifier-master directory

--- Bayes-classifier-and-Nearest-Neighbour-classifier-master/problem_1/accuracy.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-# Function to calculate the accuracy
-def accuracy(Y_pred, Y_test):
-    accuracy_vector = (Y_pred == Y_test)
-    accuracy_value = np.sum(accuracy_vector) / len(accuracy_vector)
-
-    return accuracy_value
